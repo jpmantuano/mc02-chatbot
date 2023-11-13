925a0ee9644847ab8da7ec13ca5b3291c3a4aa0a
jpmantuano <jpmantuano@Josephs-MacBook-Pro.local>
2023-11-13 15:24:34 +0800

    creates sentence class to check if input sentence is valid and get nouns

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-import SentenceDetector
+from InputSentence import InputSentence
 
 app = Flask(__name__)
 
@@ -8,14 +8,17 @@ app = Flask(__name__)
 @app.route('/sentence', methods=["POST"])
 def detect_sentence():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
+    if content_type == 'application/json':
         json = request.get_json()
         # create prolog query or statement, then return answer
-        is_question = SentenceDetector.parse_sentence(json['sentence'])
-        if is_question:
-            return {"response": "sentence is a question"}
+        input_sentence = InputSentence(json['sentence'])
+        # is_question = SentenceDetector.parse_sentence(json['sentence'])
+        if input_sentence.is_question():
+            return {"response": "sentence is a question",
+                    "tokens": input_sentence.get_tokens()}
         else:
-            return {"response": "added to knowledge base"}
+            return {"response": "added to knowledge base",
+                    "tokens": input_sentence.get_tokens()}
     else:
         return {"response": "Content-Type not supported!"}
 
