a6df1146a6009915d42c3f9da6cb7d47dd7c997e
jpmantuano <jpmantuano@Josephs-MacBook-Pro.local>
2023-11-21 09:51:44 +0800

    added sentence pattern detection

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from string import Template
+
 from flask import Flask, request
 
 from InputSentence import InputSentence
+import query_templates
 
 app = Flask(__name__)
 
@@ -13,12 +16,25 @@ def detect_sentence():
         # create prolog query or statement, then return answer
         input_sentence = InputSentence(json['sentence'])
         # is_question = SentenceDetector.parse_sentence(json['sentence'])
+        token_sequence = []
         if input_sentence.is_question():
+            query = Template('ffather(x, $x)')
+            for token in input_sentence.get_tokens():
+                token_sequence.append(token[1])
+                # if str(token[1]) == "NNP":
+                #     query.substitute(x=token[0])
+
+            # print("-".join(token_sequence))
+
             return {"response": "sentence is a question",
-                    "tokens": input_sentence.get_tokens()}
+                    "tokens": input_sentence.get_tokens(),
+                    "sentence_pattern": token_sequence,
+                    "query": query.__str__()}
         else:
             return {"response": "added to knowledge base",
-                    "tokens": input_sentence.get_tokens()}
+                    "tokens": input_sentence.get_tokens(),
+                    "sentence_pattern": token_sequence,
+                    "query": query_templates.father_of}
     else:
         return {"response": "Content-Type not supported!"}
 
